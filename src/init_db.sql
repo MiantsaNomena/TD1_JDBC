09fc790fa6a99bafd2a5c4ed2c0efc8a5b15afb0
Miantsa <hei.miantsa.3@gmail.com>
2025-12-09 22:40:49 +0300

    rectification , data.sql

--- src/init_db.sql
+++ src/init_db.sql
@@ -2,4 +2,6 @@ create database product_management_db;
 
 create user product_manager_user with password '123456';
 
-grant all privileges on database product_management_db to product_manager_user;
+grant all privileges on database  product_management_db to product_manager_user;
+
+grant  usage , create on schema public to product_manager_user;
